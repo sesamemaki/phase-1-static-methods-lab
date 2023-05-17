0f2bb8ad7299b6bb7629a40460d1fa988c8a24c8
Burcu Bayraö <burju7@yahoo.com>
2023-05-17 13:55:46 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^a-zA-Z0-9\-\'\s]/g, "");
+  }
+
+  static titleize(string) {
+    let arr = [];
+    let exludeWords = [
+      "the",
+      "a",
+      "an",
+      "but",
+      "of",
+      "and",
+      "for",
+      "at",
+      "by",
+      "and",
+      "from",
+    ];
+    arr = string.split(" ");
+    return arr
+      .map((word, i) => {
+        return exludeWords.includes(word) && i != 0
+          ? [word]
+          : word.charAt(0).toUpperCase() + word.slice(1);
+      })
+      .join(" ");
+  }
 }
+
+console.log(Formatter.capitalize("crocodile"));
+console.log(Formatter.sanitize("Doin' Dishes"));
+console.log(Formatter.titleize("Maurice a an but of and for at by from end"));
+console.log(Formatter.titleize("a tale of two cities"));
+
